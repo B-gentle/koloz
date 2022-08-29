2b2d5b4db2152eb5c0784b996d52e475ae0f8171
beetech <brighteyo16@gmail.com>
2022-08-29 13:49:32 +0100

    added the navbar scroll effect to scroll to each section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,7 @@ import './navbar.css';
 import logo from './../../assets/logo.png';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { GrClose } from 'react-icons/gr';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -19,11 +20,11 @@ const Navbar = () => {
                 <img src={logo} alt='koloz logo' />
             </div>
                 <span className='close' onClick={()=>{setDisplay(false)}}><GrClose /></span>
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Gallery</li>
-                <li>Contact</li>
+                <Link spy={true} to='navbar' smooth={true} activeClass='activeClass'><li>Home</li></Link>
+                <Link spy={true} to='about-us' smooth={true} activeClass='activeClass'><li>About</li></Link>
+                <Link spy={true} to='services' smooth={true} activeClass='activeClass'><li>Services</li></Link>
+                <Link spy={true} to='gallery' smooth={true} activeClass='activeClass'><li>Gallery</li></Link>
+                <Link spy={true} to='contact-us' smooth={true} activeClass='activeClass'><li>Contact</li></Link> 
             </ul>
             <span className='hamburger' onClick={()=>{setDisplay(true)}}><GiHamburgerMenu/></span>
         </nav>
